da550bdcd01d57b869cf220577bbb59b8f8c0871
Clay Harmon <clay@clayharmon.me>
2021-06-09 11:54:55 +0300

    Setup lingui, setup gatsby plugins, setup i18n

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,23 @@ module.exports = {
     title: "Erciyes English Website",
     siteUrl: "https://erciyes-english.github.io/ee-website/",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-theme-i18n`,
+      options: {
+        defaultLang: `en`,
+        prefixDefault: true,
+        locales: `en tr`,
+        configPath: require.resolve(`./i18n/config.json`),
+      },
+    },
+    {
+      resolve: `gatsby-theme-i18n-lingui`,
+      options: {
+        localeDir: `./i18n/lingui`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-mdx`,
+  ],
 };
