d73d58ac2a2281a3b2f18b5de7f077f34bceabd1
Clay Harmon <clay@clayharmon.me>
2021-06-09 11:49:53 +0300

    Add site url to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   pathPrefix: "/ee-website",
   siteMetadata: {
     title: "Erciyes English Website",
+    siteUrl: "https://erciyes-english.github.io/ee-website/",
   },
   plugins: [],
 };
