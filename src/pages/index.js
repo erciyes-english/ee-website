ad8642df3a3bdc5fc72285d42674f5ffba95bab9
Clay Harmon <clay@clayharmon.me>
2021-06-09 17:59:48 +0300

    Remove node-fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import fetch from "node-fetch";
+
 // styles
 const pageStyles = {
   color: "#232129",
@@ -125,32 +125,8 @@ const links = [
   },
 ];
 
-async function postData(url = "", data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "no-cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "omit", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
 // markup
 const IndexPage = () => {
-  postData(
-    "https://europe-west3-erciyes-english.cloudfunctions.net/form-processing",
-    { answer: 42 }
-  ).then((data) => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
