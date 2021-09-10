9bddb0a718f2530d6838ef8ec80fdcc30fc757a4
Clay Harmon <clay@clayharmon.me>
2021-09-10 09:53:11 +0300

    Add specific urls to lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,23 @@
+const url = (lang, page) => `http://localhost/${lang}${page}/`;
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: "./public",
+      url: [
+        url("en", ""),
+        url("tr", ""),
+        url("en", "/register"),
+        url("tr", "/register"),
+      ],
+    },
     upload: {
       target: "temporary-public-storage",
     },
     assert: {
       preset: "lighthouse:no-pwa",
+      assertions: {
+        "csp-xss": "warn",
+      },
     },
   },
 };
